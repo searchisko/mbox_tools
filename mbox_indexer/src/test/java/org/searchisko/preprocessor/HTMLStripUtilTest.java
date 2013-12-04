877aebe4b0455cdea2e3a18b6b1669829578b9d3
Lukas Vlcek <lukas.vlcek@gmail.com>
2013-12-04 16:52:25 +0100

    fix test

--- mbox_indexer/src/test/java/org/searchisko/preprocessor/HTMLStripUtilTest.java
+++ mbox_indexer/src/test/java/org/searchisko/preprocessor/HTMLStripUtilTest.java
@@ -25,12 +25,12 @@ public class HTMLStripUtilTest {
 	@Test
 	public void shouldStripHTML() {
 		String html = "<p><h1>Title</h1>Dummy<br><i>text</i></p><p><ul><li>A<li>B<li>C</ul</p>";
-		assertEquals("Title Dummy text A B C ", HTMLStripUtil.stripHTML(html));
+		assertEquals("Title Dummy text A B C", HTMLStripUtil.stripHTML(html));
 	}
 
 	@Test
 	public void shouldIgnoreScript() {
 		String html = "<div>There is<script>var x = {};</script> no script</div>";
-		assertEquals("There is no script ", HTMLStripUtil.stripHTML(html));
+		assertEquals("There is no script", HTMLStripUtil.stripHTML(html));
 	}
 }
